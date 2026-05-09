46b91184e30c25966975bcce7f2975a60f2a7b3f
aryansh35 <gupta.aryansh135@gmail.com>
2026-05-10 02:51:37 +0530

    Mesh UI and Mobile Restoration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col relative">
+        <div className="mesh-bg">
+          <div className="mesh-orb mesh-orb-1" />
+          <div className="mesh-orb mesh-orb-2" />
+        </div>
         {children}
         <Script id="pwa-register" strategy="afterInteractive">
           {`
